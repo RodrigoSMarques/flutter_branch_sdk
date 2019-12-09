a5b8c99f533b2b273793df989b8678786b04f707
Rodrigo de Souza Marques <rodrigosmarques@gmail.com>
2019-12-09 14:26:56 -0300

    Update branch_universal_object.dart

--- lib/src/branch_universal_object.dart
+++ lib/src/branch_universal_object.dart
@@ -9,22 +9,31 @@ part of flutter_branch_sdk;
 class BranchUniversalObject {
   /* Canonical identifier for the content referred. */
   String canonicalIdentifier = '';
+
   /* Canonical url for the content referred. This would be the corresponding website URL */
   String canonicalUrl = '';
+
   /* Title for the content referred by BranchUniversalObject */
   String title = '';
+
   /* Description for the content referred */
   String contentDescription = '';
+
   /* An image url associated with the content referred */
   String imageUrl = '';
+
   /* Meta data provided for the content. {@link ContentMetadata} object holds the metadata for this content */
   BranchContentMetaData contentMetadata = BranchContentMetaData();
+
   /* Content index mode */
   bool publiclyIndex = true;
+
   /* Any keyword associated with the content. Used for indexing */
   List<dynamic> keywords = List();
+
   /* Expiry date for the content and any associated links. Represented as epoch milli second */
   int expirationDateInMilliSec = 0;
+
   /* Index mode for  local content indexing */
   bool locallyIndex = true;
   int _creationDateTimeStamp = DateTime.now().millisecondsSinceEpoch;
@@ -37,10 +46,10 @@ class BranchUniversalObject {
       this.contentDescription,
       this.imageUrl,
       this.contentMetadata,
-        this.keywords,
+      this.keywords,
       this.publiclyIndex,
-      this.expirationDateInMilliSec,
-      this.locallyIndex});
+      this.locallyIndex,
+      this.expirationDateInMilliSec});
 
   ///Adds any keywords associated with the content referred
   void addKeyWords(List<dynamic> keywords) {
