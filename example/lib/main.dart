92a5c74af2ad878f03c168878ec945332454baaf
Rodrigo de Souza Marques <rodrigosmarques@gmail.com>
2022-06-29 00:14:47 -0300

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -330,7 +330,6 @@ class _HomePageState extends State<HomePage> {
   void generateQrCode(
     BuildContext context,
   ) async {
-    /*
     BranchResponse responseQrCodeData = await FlutterBranchSdk.getQRCodeAsData(
         buo: buo!,
         linkProperties: lp,
@@ -345,8 +344,7 @@ class _HomePageState extends State<HomePage> {
       print(
           'Error : ${responseQrCodeData.errorCode} - ${responseQrCodeData.errorMessage}');
     }
-    */
-
+    /*
     BranchResponse responseQrCodeImage =
         await FlutterBranchSdk.getQRCodeAsImage(
             buo: buo!,
@@ -364,6 +362,8 @@ class _HomePageState extends State<HomePage> {
           message:
               'Error : ${responseQrCodeImage.errorCode} - ${responseQrCodeImage.errorMessage}');
     }
+
+   */
   }
 
   void showGeneratedLink(BuildContext context, String url) async {
