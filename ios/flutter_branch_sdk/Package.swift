f8c49ae71591f0ddba559e294f0d47dc17d5ba30
Rodrigo de Souza Marques <rodrigosmarques@gmail.com>
2024-12-16 01:10:45 -0300

    Add Support do SPM

--- ios/flutter_branch_sdk/Package.swift
+++ ios/flutter_branch_sdk/Package.swift
@@ -2,6 +2,49 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+/*
+import Foundation
+
+enum ConfigurationError: Error {
+  case fileNotFound(String)
+  case parsingError(String)
+  case invalidFormat(String)
+}
+
+let branchDirectory = String(URL(string: #file)!.deletingLastPathComponent().absoluteString
+  .dropLast())
+
+print(branchDirectory);
+
+func loadPubspecVersions() throws -> String {
+  let pubspecPath = NSString.path(withComponents: [branchDirectory,"..","..","pubspec.yaml"])
+    print(pubspecPath);
+  do {
+    let yamlString = try String(contentsOfFile: pubspecPath, encoding: .utf8)
+    let lines = yamlString.split(separator: "\n")
+
+    guard let packageVersionLine = lines.first(where: { $0.starts(with: "version:") }) else {
+      throw ConfigurationError.invalidFormat("No package version line found in pubspec.yaml")
+    }
+    var packageVersion = packageVersionLine.split(separator: ":")[1]
+      .trimmingCharacters(in: .whitespaces)
+      .replacingOccurrences(of: "+", with: "-")
+    packageVersion = packageVersion.replacingOccurrences(of: "^", with: "")
+    return packageVersion
+  } catch {
+    throw ConfigurationError.fileNotFound("Error loading or parsing pubspec.yaml: \(error)")
+  }
+}
+
+let library_version: String
+
+do {
+  library_version = try loadPubspecVersions()
+} catch {
+  fatalError("Failed to load configuration: \(error)")
+}
+*/
+
 
 let package = Package(
     name: "flutter_branch_sdk",
@@ -20,6 +63,9 @@ let package = Package(
             dependencies: [
             .product(name: "BranchSDK", package: "ios-branch-sdk-spm"),
             ],
+            //cSettings: [
+            //        .define("LIBRARY_VERSION", to: "\"\(library_version)\""),
+            //],
             linkerSettings: [
                 .linkedFramework("CoreServices"),
                 .linkedFramework("SystemConfiguration"),
